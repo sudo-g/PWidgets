4f5909d82f9a2c69656c135e22a8bb7a251d1e22
George Xian <george_xian@live.com>
2015-04-06 21:56:19 +1200

    Changed joystick to suit new FSM library

--- src/com/tronacademy/phantom/controls/Joystick.java
+++ src/com/tronacademy/phantom/controls/Joystick.java
@@ -7,10 +7,10 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.tronacademy.phantom.fsm.EventSpace;
 import com.tronacademy.phantom.fsm.FsmEvent;
 import com.tronacademy.phantom.fsm.FsmState;
 import com.tronacademy.phantom.utils.ControlInput;
@@ -35,12 +35,12 @@ public class Joystick extends View implements ControlInput {
 	private static final int NUM_OF_SUBCHANS = 2;
 	
 	// FSM properties
-	private static final int EV_SPACE_ID = 1;
-	private static final int EV_SPACE_SIZE = 4;
-	protected static final FsmEvent EVENT_GRIP = new FsmEvent("Grip", EV_SPACE_ID, 0);
-	protected static final FsmEvent EVENT_RELEASE = new FsmEvent("Release", EV_SPACE_ID, 1);
-	protected static final FsmEvent EVENT_INBOUNDARY = new FsmEvent("OnBoundary", EV_SPACE_ID, 2);
-	protected static final FsmEvent EVENT_ONBOUNDARY = new FsmEvent("InBoundary", EV_SPACE_ID, 3);
+	private static final int JOYSTICK_EVSP_SIZE = 4;
+	private static final EventSpace JOYSTICK_EVSP = new EventSpace(JOYSTICK_EVSP_SIZE);
+	protected static final FsmEvent EVENT_GRIP = JOYSTICK_EVSP.newEvent("Grip");
+	protected static final FsmEvent EVENT_RELEASE = JOYSTICK_EVSP.newEvent("Release");
+	protected static final FsmEvent EVENT_INBOUNDARY = JOYSTICK_EVSP.newEvent("In Boundary");
+	protected static final FsmEvent EVENT_ONBOUNDARY = JOYSTICK_EVSP.newEvent("On Boundary");
 	
 	/* -- Fields -- */
 	public String mName = "Joystick";
@@ -340,37 +340,33 @@ public class Joystick extends View implements ControlInput {
 	private class JoystickFsm extends FsmState {
 		
 		// worker states for joy-stick
-		private IdleState mIdleState = new IdleState(EV_SPACE_ID, EV_SPACE_SIZE);
-		private InBoundaryState mInBoundaryState = new InBoundaryState(EV_SPACE_ID, EV_SPACE_SIZE);
-		private OnBoundaryState mOnBoundaryState = new OnBoundaryState(EV_SPACE_ID, EV_SPACE_SIZE);
+		private IdleState mIdleState = new IdleState();
+		private InBoundaryState mInBoundaryState = new InBoundaryState();
+		private OnBoundaryState mOnBoundaryState = new OnBoundaryState();
 		
 		public JoystickFsm() {
-			// this is the hosting state and thus it has a 0 event space because it
-			// always defers control to sub-states
-			super("Joystick", 0, 0);
+			// Hosting state listens to 0 size event space 
+			// as it always delegates control to internal states,
+			// of which it has a non-zero internal state space.
+			super("Joystick", new EventSpace(0), JOYSTICK_EVSP_SIZE);
 			
-			try {
-				setEntrySubstate(mIdleState);
-			} catch (IllegalArgumentException e) {
-				Log.e(TAG, "Event space ID invalid");
-			} catch (InterruptedException e) {
-				Log.e(TAG, "Attempted to set entry sub-state more than once");
-			}
+			mIdleState.linkStates();
+			mInBoundaryState.linkStates();
+			mOnBoundaryState.linkStates();
 			
-			// couple states
-			mIdleState.bindEventToTransition(mInBoundaryState, EVENT_GRIP);
-			
-			mInBoundaryState.bindEventToTransition(mIdleState, EVENT_RELEASE);
-			mInBoundaryState.bindEventToTransition(mOnBoundaryState, EVENT_ONBOUNDARY);
-			
-			mOnBoundaryState.bindEventToTransition(mIdleState, EVENT_RELEASE);
-			mOnBoundaryState.bindEventToTransition(mInBoundaryState, EVENT_INBOUNDARY);
+			addStateToInternalFsm(mIdleState);
+			addStateToInternalFsm(mInBoundaryState);
+			addStateToInternalFsm(mOnBoundaryState);
 		}
 		
 		/* -- Worker states -- */
 		private class IdleState extends FsmState {
-			public IdleState(int evSpaceId, int evSpaceSize) {
-				super("Idle", evSpaceId, evSpaceSize);
+			public IdleState() {
+				super("Idle", JOYSTICK_EVSP, 0);
+			}
+			
+			void linkStates() {
+				bindEventToTransition(mInBoundaryState, EVENT_GRIP);
 			}
 			
 			public void performAction(Object... context) {
@@ -390,8 +386,13 @@ public class Joystick extends View implements ControlInput {
 		}
 		
 		private class InBoundaryState extends FsmState {
-			public InBoundaryState(int evSpaceId, int evSpaceSize) {
-				super("In Boundary", evSpaceId, evSpaceSize);
+			public InBoundaryState() {
+				super("In Boundary", JOYSTICK_EVSP, 0);
+			}
+			
+			void linkStates() {
+				bindEventToTransition(mIdleState, EVENT_RELEASE);
+				bindEventToTransition(mOnBoundaryState, EVENT_ONBOUNDARY);	
 			}
 			
 			public void performAction(Object... context) {
@@ -416,8 +417,13 @@ public class Joystick extends View implements ControlInput {
 		}
 		
 		private class OnBoundaryState extends FsmState {
-			public OnBoundaryState(int evSpaceId, int evSpaceSize) {
-				super("On Boundary", evSpaceId, evSpaceSize);
+			public OnBoundaryState() {
+				super("On Boundary", JOYSTICK_EVSP, 0);
+			}
+			
+			void linkStates() {
+				bindEventToTransition(mIdleState, EVENT_RELEASE);
+				bindEventToTransition(mInBoundaryState, EVENT_INBOUNDARY);
 			}
 			
 			public void performAction(Object... context) {
